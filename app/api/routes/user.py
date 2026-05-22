ec9fbde7bb8a8133b3887941326f6a968e7cf2ba
Casadka <vasylshchur05@gmail.com>
2026-05-22 18:39:33 +0300

    add update_user endpoint and  get_all_users_info logic

--- app/api/routes/user.py
+++ app/api/routes/user.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter,Depends,status,HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from typing import List
 from app.dependencies.db import get_db
 from app.dependencies.user import allow_staff,allow_admin
 from app.repositories.user import UserRepository
-from app.schemas.user import UserCreate,UserBase
+from app.schemas.user import UserCreate,UserInfo,UserUpdate
 from uuid import  UUID
 
 router = APIRouter()
@@ -27,11 +27,31 @@ async def delete_user(user_id: UUID, db: AsyncSession = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not Found")
     return None
 
-@router.get("/get_user_info",response_model= UserBase,dependencies=[Depends(allow_staff)])
-async def get_info(user_id: UUID, db: AsyncSession = Depends(get_db)):
+@router.get("/get_user_info",response_model=UserInfo,dependencies=[Depends(allow_staff)])
+async def get_user_info(user_id: UUID, db: AsyncSession = Depends(get_db)):
     user_repo = UserRepository(db)
     user = await user_repo.get_by_id(user_id)
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="User not Found")
     return user
+
+@router.get("/get_all_users_info", response_model=List[UserInfo], dependencies=[Depends(allow_staff)])
+async def get_all_users_info(limit: int = 50, offset: int = 0, db: AsyncSession = Depends(get_db)):
+    user_repo = UserRepository(db)
+    users = await user_repo.get_all(limit=limit, offset=offset)
+
+    if not users:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Users not Found")  
+    return users
+
+
+@router.patch("/update", response_model=UserInfo, dependencies=[Depends(allow_admin)])
+async def update_user(user_id: UUID, user_data: UserUpdate, db: AsyncSession = Depends(get_db)):
+    user_repo = UserRepository(db)
+    updated_user = await user_repo.update(user_id, user_data.model_dump(exclude_unset=True))
+    
+    if not updated_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    return updated_user

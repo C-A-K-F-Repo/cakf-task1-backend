fc1385fac60b92e08d4b5b05d073437c02fe3923
Samopadnyi Oleg <olegsannopadnui27027612@gmail.com>
2026-05-25 21:45:59 +0300

    deletion product feat

--- app/api/routes/products.py
+++ app/api/routes/products.py
@@ -33,3 +33,8 @@ async def get_product(product_id: uuid.UUID, db: SessionDep):
     if not product:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
     return product
+
+
+@router.delete("/{product_id}", status_code=status.HTTP_204_NO_CONTENT, dependencies=[Depends(allow_staff)])
+async def delete_product(product_id: uuid.UUID, db: SessionDep):
+    await ProductRepository(db).delete(product_id)
